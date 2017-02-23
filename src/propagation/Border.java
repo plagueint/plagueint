c7af79cf081c422704b45319b5563ce615d29933
occuria <stevan.coroller@telecom-sudparis.eu>
2017-02-23 18:54:57 +0100

    Ajout classe Border

---
+++ src/propagation/Border.java
@@ -0,0 +1,10 @@
+package propagation;
+
+public class Border {
+	private double id;
+	private double susceptibles;
+	private double infectives;
+	private double recovered;
+	private double population;
+	private float hygieneRate;
+}
