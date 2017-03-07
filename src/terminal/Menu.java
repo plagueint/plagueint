876bcdaeac5d70afc05c5cc723a8c31a51b18d5e
varenspukis <romain.cherre@telecom-sudparis.eu>
2017-03-07 09:13:05 +0100

    start of menu implementation

--- src/terminal/Menu.java
+++ src/terminal/Menu.java
@@ -1,5 +1,46 @@
 package terminal;
 
+import java.io.Console;
+
 public class Menu {
+	
+	
+	final private static Console console=System.console();
+	
+	float getFloat(){
+		String s="";
+		float number=0;
+		boolean ask=true;
+		while (ask){
+			try{
+				s=console.readLine();
+				number=Float.parseFloat(s);
+				ask=false;
+			} catch (NumberFormatException e){
+				System.out.println(s + "is not a float");
+				ask=true;
+			}
+		}
+		return number;
+	}
+	
+	String getString(){
+		String s="";
+		boolean ask=true;
+		while (ask){
+			try{
+				s=console.readLine();
+				ask=false;
+			} catch (Exception e){
+				System.out.println(s + "is not correct");
+				ask=true;
+			}
+		}
+		return "";
+	}
+	
+	double getDouble(){
+		return 0;
+	}
 
 }
