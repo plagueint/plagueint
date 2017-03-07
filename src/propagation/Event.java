ba7886960fc21c5b0b081b3b61c37a91ae397df7
occuria <stevan.coroller@telecom-sudparis.eu>
2017-03-07 13:12:37 +0100

    Déclaration des attributs de Event

--- src/propagation/Event.java
+++ src/propagation/Event.java
@@ -1,8 +1,35 @@
 package propagation;
+import java.util.Hashtable;
 
 public abstract class Event {
-	// La classe event contient le nom d'un évenement, le temps
+	public Event(String name, double time, Hashtable<String, Float> parameters) {
+		super();
+		this.name = name;
+		this.time = time;
+		this.parameters = parameters;
+	}
+	
 	private String name;
 	private double time;
-	private Map<String, float> M= new HashMap<String, float>();
+	private Hashtable<String , Float> parameters;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getTime() {
+		return time;
+	}
+	public void setTime(double time) {
+		this.time = time;
+	}
+	public Hashtable<String, Float> getParameters() {
+		return parameters;
+	}
+	public void setParameters(Hashtable<String, Float> parameters) {
+		this.parameters = parameters;
+	}
+	
 }
