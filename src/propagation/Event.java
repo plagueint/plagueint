c7ae1e7c23088b7eab10921ebf8795ff8b5ea76a
occuria <stevan.coroller@telecom-sudparis.eu>
2017-03-07 10:44:22 +0100

    Début classe Event

--- src/propagation/Event.java
+++ src/propagation/Event.java
@@ -2,5 +2,7 @@ package propagation;
 
 public abstract class Event {
 	// La classe event contient le nom d'un évenement, le temps
-	
+	private String name;
+	private double time;
+	private Map<String, float> M= new HashMap<String, float>();
 }
--- src/terminal/Menu.java
+++ src/terminal/Menu.java
@@ -54,5 +54,6 @@ public class Menu {
 			}
 		}
 		return number;
-	}		
+	}
+	
 }
