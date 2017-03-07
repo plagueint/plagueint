4a43544dd417e4bdd55864041091ab1ab328355f
varenspukis <romain.cherre@telecom-sudparis.eu>
2017-03-07 13:57:41 +0100

    Ajout fonction ordre superieur au menu

--- src/terminal/Menu.java
+++ src/terminal/Menu.java
@@ -1,13 +1,19 @@
 package terminal;
-
 import java.io.Console;
+import java.util.function.Function;
+import propagation.GenericModel;
 
 public class Menu {
 	
+	GenericModel model;
 	
 	final private static Console console=System.console();
 	
-	float getFloat(){
+	/*
+	 * Prend en argument la fonction qui change la valeur de l'attribut souhaité
+	 * ex : voir la permière fonction sous les getters simples
+	*/
+	float getFloat( Function <Float,Void> function){
 		String s="";
 		float number=0;
 		boolean ask=true;
@@ -16,9 +22,12 @@ public class Menu {
 				s=console.readLine();
 				number=Float.parseFloat(s);
 				ask=false;
+				function.apply(number);
 			} catch (NumberFormatException e){
 				System.out.println(s + "is not a float");
 				ask=true;
+			} catch (Exception e){
+				System.out.println(e.getMessage());
 			}
 		}
 		return number;
@@ -54,5 +63,30 @@ public class Menu {
 			}
 		}
 		return number;
-	}		
+	}
+	
+	
+	/*
+	 * Fonction exemple 
+	 */
+	void menuPrincipal(){
+		String s="";// La chaine de caractère que l'utilisateur rentre
+		boolean ask=true;
+		int number=0;// Le nombre que l'utilisateur veut rentrer
+		while(ask){
+			try{
+				s=console.readLine();
+				ask=false;
+				number = Integer.parseInt(s);
+			} catch (NumberFormatException e){
+				System.out.println(s + "is not an integer");
+				ask=true;
+			}
+		}
+		
+		switch (number){
+			case 1:getFloat(model.setInfectives);
+		}
+	}
+	
 }
