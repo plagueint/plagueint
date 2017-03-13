f90f677e2f23bc5e31d495149d4c7f689ff94d8c
Pierrick Pamart <pierrick.pamart@telecom-sudparis.eu>
2017-03-13 22:19:03 +0100

    Ajout classe Csv

---
+++ src/DAO/Csv.java
@@ -0,0 +1,18 @@
+package DAO;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Csv {
+	public static ArrayList<String> read(String file, String separator, int[] infoPosition){
+		
+		
+		
+		
+		ArrayList<String> a = new ArrayList<String>(2);
+		return a;
+	}
+}
