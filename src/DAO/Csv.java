e08d9c8f75cd2de4d6af32319d64cf1587b0a271
Pierrick Pamart <pierrick.pamart@telecom-sudparis.eu>
2017-04-16 15:39:42 +0200

    Csv.read is ready

--- src/DAO/Csv.java
+++ src/DAO/Csv.java
@@ -7,12 +7,57 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Csv {
-	public static ArrayList<String> read(String file, String separator, int[] infoPosition){
+	public static ArrayList<String[]> read(String file, String separator, int[] infoPosition){ 
+		/*
+		 * Retourne sous forme de tableau les données d'un Csv
+		 * 
+		 * file : fichier Csv
+		 * separator : séparateur des données
+		 * infoPosition : position des infos à récupérer
+		 */
 		
 		
+		ArrayList<String[]> table = new ArrayList<String[]>(2);
+		BufferedReader br = null ;
+		String line = "" ;
+		int nud = infoPosition.length;				// Number of Useful Data
 		
 		
-		ArrayList<String> a = new ArrayList<String>(2);
-		return a;
+		try {
+
+            br = new BufferedReader(new FileReader(file));
+            
+            String[] useful_data = new String[nud];	// Tableau temporaire où l'on stocke les données de la ligne
+            
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(separator);  // On split la ligne dans un tableau
+                
+                for (int i = 0; i < nud; i++) {
+					useful_data[i] = data[infoPosition[i]];
+				}
+                System.out.println(useful_data[0]);
+                table.add(useful_data);
+
+            }
+
+// Début de la partie empruntée
+        } catch (FileNotFoundException e) {  
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+// Fin de la partie empruntée
+		
+		return table;
 	}
+	
 }
